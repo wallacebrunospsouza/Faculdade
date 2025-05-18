830fed33719ece85cfe7a3f50435dd14caf8a68a
Wrube <wallacebrunospsouza82@gmail.com>
2025-05-18 17:48:34 -0300

    feito

--- Programação I/Lista I/src/Q1.java
+++ Programação I/Lista I/src/Q1.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Q1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escreva o primeiro número:");
+        Float N1 = entrada.nextFloat();
+
+        System.out.println("Escreva o segundo número:");
+        Float N2 = entrada.nextFloat();
+
+        System.out.println("Escreva o terceiro número:");
+        Float N3 = entrada.nextFloat();
+
+        Float med = N1 + N2 + N3 / 3;
+
+        System.out.println("A média é: " + med);
+
+
+
     }
 }
