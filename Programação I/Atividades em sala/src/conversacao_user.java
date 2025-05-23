6c7fad9ea768a05de5ff9075397049ee1db6ee92
Wrube <wallacebrunospsouza82@gmail.com>
2025-05-23 09:09:29 -0300

    Primeira atividade em sala

---
+++ Programação I/Atividades em sala/src/conversacao_user.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class conversacao_user {
+   public static void main(String []args){
+     String nome;
+     double peso;
+     int idade;
+    boolean fumante;
+    Scanner teclado = new Scanner (System.in);
+    
+    System.out.println("Digite seu nome:");
+    nome = teclado.nextLine();
+    
+    System.out.println("Digite seu peso:");
+    peso = teclado.nextDouble();
+    
+    System.out.println("Digite sua idade:");
+    idade = teclado.nextInt();
+    
+    System.out.println("Digite se você fumante ou não (True=sim ou False=não :");
+    fumante = teclado.nextBoolean();
+   
+     //Usando Println
+     System.out.println("Meu nome é" +nome+ "minha idade é" +idade+ "anos");
+     System.out.println ("Peso" +peso+ "kg e tabagista" +fumante);
+     
+      
+   }
+}
