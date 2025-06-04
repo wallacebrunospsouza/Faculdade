deb253d96f5c28fd71e15cd7ab543909df42822a
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-04 19:59:19 -0300

    Iniciando a lista de condicionais

---
+++ Programação I/Lista I - Condicionais/src/Questao_1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Questao_1 {
+    public static void main(String[] args) throws Exception {
+        int idade;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Escreva sua idade:");
+        idade = in.nextInt();
+
+        if( idade >= 5 || idade <= 7){
+
+            System.out.println("Infantil");
+            in.close();
+
+        } else if( idade >= 5 || idade <= 7){
+
+            System.out.println("Infantil");
+            in.close();
+
+        } if( idade >= 5 || idade <= 7){
+
+            System.out.println("Infantil");
+            in.close();
+
+        } if( idade >= 5 || idade <= 7){
+
+            System.out.println("Infantil");
+            in.close();
+
+        } if( idade >= 5 || idade <= 7){
+
+            System.out.println("Infantil");
+            in.close();
+
+        }
+    }
+}
