cf2f828a9eff9332954fc9ba81269049f2cddf36
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-12 13:13:45 -0300

    Ajusta a lógica da classe Questao_14 para verificar corretamente se um número é um palíndromo, corrigindo a ordem de multiplicação e a comparação.

--- Programação I/Lista - Condicionais/src/Questao_14.java
+++ Programação I/Lista - Condicionais/src/Questao_14.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class Questao_14 {
     public static void main(String[] args){
-        int num, casa_1, casa_2, casa_3, num2;
+        int num, casa_1, casa_2, casa_3, num2, Re_1, Re_2, Re_3;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Escreva seu número:");
         num = in.nextInt();
 
         casa_1 = num%10;
+        Re_1 = num/10;
         casa_2 = casa_1%10;
+        Re_2 = Re_1/10;
         casa_3 = casa_2%10;
-        num2 = (casa_1*1) + (casa_2*10) + (casa_3*100);
+        Re_3 = Re_2/10;
+        num2 = (casa_1*100) + (casa_2*10) + (casa_3*1);
 
 
-        if (num == num2){
+        if (num2 == num){
             System.out.println("Seu número é um Palíndromo");
         } else {
-            System.out.println("Seu número não é um Palíndormo");
+            System.out.println("Seu número não é um Palíndromo");
             in.close();
         }
 
