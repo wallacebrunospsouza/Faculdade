80062993e8d5910a68fd94f621ab0007ac70763a
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-12 07:22:52 -0300

    renomeado a pasta da lista de condicionais.

---
+++ Programação I/Lista - Condicionais/src/Questao_12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Questao_12 {
+    public static void main(String[] args){
+        int data_vot, data_nasc;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Escreva sua data de nascimento:");
+        data_nasc = in.nextInt();
+        System.out.println("Escreva a data da sua votação:");
+        data_vot = in.nextInt();
+
+        
+    }
+
+}
