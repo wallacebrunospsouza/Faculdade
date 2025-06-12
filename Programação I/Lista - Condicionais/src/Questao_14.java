82e7f4029d50e658a03d1fe357130bb6133aa14e
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-12 13:22:07 -0300

    Refatora a classe Questao_14 para corrigir a lógica de verificação de palíndromos, removendo variáveis desnecessárias e ajustando a ordem de multiplicação.

--- Programação I/Lista - Condicionais/src/Questao_14.java
+++ Programação I/Lista - Condicionais/src/Questao_14.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class Questao_14 {
     public static void main(String[] args){
-        int num, casa_1, casa_2, casa_3, num2, Re_1, Re_2, Re_3;
+        int num, casa_1, casa_2, casa_3, num2;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Escreva seu número:");
         num = in.nextInt();
 
         casa_1 = num%10;
-        Re_1 = num/10;
         casa_2 = casa_1%10;
-        Re_2 = Re_1/10;
         casa_3 = casa_2%10;
-        Re_3 = Re_2/10;
-        num2 = (casa_1*100) + (casa_2*10) + (casa_3*1);
+        num2 = (casa_1*1) + (casa_2*10) + (casa_3*100);
 
 
         if (num2 == num){
