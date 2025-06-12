eb2b7cd9ada85cf2c74e544af53b9ccb6da7af23
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-12 13:27:50 -0300

    Implementa a classe Questao_15 e ajusta a classe Questao_14 para corrigir a lógica de verificação de palíndromos.

--- Programação I/Lista - Condicionais/src/Questao_14.java
+++ Programação I/Lista - Condicionais/src/Questao_14.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Questao_14 {
     public static void main(String[] args){
-        int num, casa_1, casa_2, casa_3, num2;
+        int num, casa_1, casa_2, casa_3, num2, Re;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Escreva seu número:");
         num = in.nextInt();
 
         casa_1 = num%10;
-        casa_2 = casa_1%10;
+        Re = num/10;
+        casa_2 = Re%10;
+        Re = Re/10;
         casa_3 = casa_2%10;
         num2 = (casa_1*1) + (casa_2*10) + (casa_3*100);
 
--- Programação I/Lista - Condicionais/src/Questao_15.java
+++ Programação I/Lista - Condicionais/src/Questao_15.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Questao_15 {
     public static void main(String[] args){
+        int num, casa_1, casa_2, casa_3, casa_4, Re;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Escreva o seu número:");
+        num = in.nextInt();
+
+        casa_1 = num%2;
+        Re = num/2;
+        casa_2 = Re%2;
+        Re = Re/2;
+        casa_3 = Re%2;
+        Re = Re/2;
+        casa_4 = Re%2;
+
         
     }
 
