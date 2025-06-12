7b3911e1ae4e96aa6d510f162898c357cc6432f3
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-12 07:37:33 -0300

    Corrige tipos de variáveis para data de nascimento e data de votação em Questao_12, alterando de int para float.

--- Programação I/Lista - Condicionais/src/Questao_12.java
+++ Programação I/Lista - Condicionais/src/Questao_12.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Questao_12 {
     public static void main(String[] args){
-        int data_vot, data_nasc;
+        float data_vot, data_nasc;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Escreva sua data de nascimento:");
-        data_nasc = in.nextInt();
+        data_nasc = in.nextFloat();
         System.out.println("Escreva a data da sua votação:");
-        data_vot = in.nextInt();
+        data_vot = in.nextFloat();
+
+        System.out.printf("Sua data de nascimento é: %,2f \n e sua data de votação é: %.2f", data_nasc, data_vot); 
+
+
 
         
     }
