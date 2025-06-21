692cbbe34cd7c5b049f8fe863e5d7e4a7800850f
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-21 19:06:51 -0300

    Adiciona lógica para cálculo de salário final com base no salário base e tempo trabalhado na Questao_19.

--- 1º Semestre/Programação I/Lista - Condicionais/src/Questao_19.java
+++ 1º Semestre/Programação I/Lista - Condicionais/src/Questao_19.java
@@ -3,6 +3,26 @@ import java.util.Scanner;
 public class Questao_19 {
     public static void main(String[] args){
         double salBase, tempo, grat, imp, salFinal;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Escreva seu sálario base:");
+        salBase = in.nextDouble();
+        System.out.println("Escreva o tempo que você trabalhou no mês:");
+        tempo = in.nextDouble();
+
+        if (salBase < 2000){
+            tempo = tempo/20;
+            grat = salBase + tempo;
+            salFinal = grat - 0.10;
+            System.out.printf("Seu salário final é %.2f", salFinal);
+        } else if (salBase >= 2000){
+            tempo = tempo/20;
+            grat = salBase + tempo;
+            salFinal = grat - 0.15;
+            System.out.printf("Seu salário final é %.2f", salFinal);
+
+        }
+        in.close();
     }
 
 }
