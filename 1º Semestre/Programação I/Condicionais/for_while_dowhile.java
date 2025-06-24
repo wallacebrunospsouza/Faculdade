7fee1a486abf748164bf9a73f4c0573581f124eb
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-24 08:21:54 -0300

    Adiciona implementação da classe for_while_dowhile com lógica de repetição para exibir mensagem de estudo.

---
+++ 1º Semestre/Programação I/Condicionais/for_while_dowhile.java
@@ -0,0 +1,10 @@
+public class for_while_dowhile{
+    public static void main(String[] args){
+        int qtd = 1;
+        while(qtd <= 100){
+            System.out.println("Eu preciso estudar mais");
+            qtd = qtd + 1;
+        }
+    }
+
+}
