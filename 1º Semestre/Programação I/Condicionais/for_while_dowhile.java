4d027c6f50715e5c910389e009074adef415bafc
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-24 08:27:38 -0300

    Atualiza a lógica de repetição na classe for_while_dowhile para permitir que o usuário defina a quantidade de execuções.

--- 1º Semestre/Programação I/Condicionais/for_while_dowhile.java
+++ 1º Semestre/Programação I/Condicionais/for_while_dowhile.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class for_while_dowhile{
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        double n;
+
+        System.out.print("Escreva a quantide de vezes que você deseja rodar: ");
+        n = in.nextDouble();
+
         int qtd = 1;
-        while(qtd <= 100){
-            System.out.println("Eu preciso estudar mais");
+        while(qtd <= n){
+            System.out.println(qtd + " Eu preciso estudar mais");
             qtd = qtd + 1;
         }
     }
