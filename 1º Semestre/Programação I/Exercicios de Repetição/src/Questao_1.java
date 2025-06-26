6402d839854b9f7755a631023ac9d97cf16f18a8
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-26 08:13:56 -0300

    adiciona arquivos de configuração e implementações iniciais para os exercícios

---
+++ 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_1.java
@@ -0,0 +1,10 @@
+public class Questao_1 {
+    public static void main(String[] args){
+        int num;
+        for(num = 1; num <= 100; num++){
+            System.out.println(num + " ");
+
+        }
+    }
+
+}
