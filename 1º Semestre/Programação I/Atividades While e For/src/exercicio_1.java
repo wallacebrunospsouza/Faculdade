4a01d6b21d69cfc914191bc2f723713fd998fac7
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-26 07:14:10 -0300

    Atualiza a lógica do exercício 1 para permitir a exibição de números específicos com base na entrada do usuário.

--- 1º Semestre/Programação I/Atividades While e For/src/exercicio_1.java
+++ 1º Semestre/Programação I/Atividades While e For/src/exercicio_1.java
@@ -4,15 +4,14 @@ public class exercicio_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double n;
-        int qtd = 1;
+        double qtd = 1;
 
         System.out.print("Escreva quantas vezes você quer escrever: ");
         n = in.nextDouble();
 
         while (qtd <= n) {
-            if (qtd == 2 * n) {
-                System.out.print(qtd + "; ");
-            }
+                System.out.printf("%.0f,%.0f; ", qtd, qtd);
+            
             qtd++;
         }
     }
