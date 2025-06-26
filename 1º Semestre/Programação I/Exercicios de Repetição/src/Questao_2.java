37c9b80e319e69b5b849e9b718f533b90e81f0ac
Wrube <wallacebrunospsouza82@gmail.com>
2025-06-26 08:22:20 -0300

    adiciona implementação inicial da classe Questao_2 com loop para impressão de números de 0 a 50

---
+++ 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_2.java
@@ -0,0 +1,10 @@
+public class Questao_2 {
+    public static void main(String[] args) {
+        int num;
+        for (num = 0; num <= 50; num++) {
+            System.out.println(num+ " ");
+
+        }
+
+    }
+}
