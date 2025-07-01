4037e34c78a287cfd582e0d5f225d052f22d55ec
Wrube <wallacebrunospsouza82@gmail.com>
2025-07-01 07:25:51 -0300

    adiciona implementação da classe Questao_3 com loop para imprimir números de 100 a 1

---
+++ 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_3.java
@@ -0,0 +1,13 @@
+public class Questao_3 {
+    public static void main(String []args){
+        int num;
+        int N = 100;
+        for(num = 0; num < 100; num++){
+            N = N - 1;
+            System.out.println(N + " ");
+            
+
+        }
+    }
+
+}
