1ac608a19b87d8d746060ecdf02f003e65560d84
Wrube <wallacebrunospsouza82@gmail.com>
2025-07-01 07:18:59 -0300

    ajusta o loop na classe Questao_2 para imprimir números de 0 a 50

--- 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_2.java
+++ 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_2.java
@@ -1,8 +1,10 @@
 public class Questao_2 {
     public static void main(String[] args) {
         int num;
-        for (num = 0; num <= 50; num++) {
-            System.out.println(num+ " ");
+        double N = 0;
+        for (num = 0; num < 25; num++) {
+            N = N + 2;
+            System.out.println(N + " ");
 
         }
 
