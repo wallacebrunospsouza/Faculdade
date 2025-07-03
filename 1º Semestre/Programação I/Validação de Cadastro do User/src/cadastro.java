45a4ec73c539ca624a0be9dc5572f5647023e239
Wrube <wallacebrunospsouza82@gmail.com>
2025-07-03 13:16:53 -0300

    refatora a classe cadastro para melhorar a validação de email e senha, removendo quebras de linha desnecessárias nas mensagens

--- 1º Semestre/Programação I/Validação de Cadastro do User/src/cadastro.java
+++ 1º Semestre/Programação I/Validação de Cadastro do User/src/cadastro.java
@@ -8,55 +8,59 @@ public class cadastro {
         Scanner in = new Scanner(System.in);
 
         do {
-            System.out.print("Escreva seu primeiro nome: \n");
+            System.out.print("Escreva seu primeiro nome: ");
             nome = in.nextLine();
             tamanho = nome.length();
         } while (tamanho < 2);
 
         do {
-            System.out.print("Escreva sua idade: \n");
+            System.out.print("Escreva sua idade: ");
             idade = in.nextInt();
         } while (idade <= 0 && idade > 150);
 
         do {
-            System.out.print("Escreva seu email: \n");
-            email = in.nextLine();
-        } while(email.contains("@") && email.contains(".com"));
+            System.out.print("Escreva seu email: ");
+            email = in.next();
+        } while (!(email.contains("@") && email.contains(".com")));
 
+        boolean especial, maiuscula, minuscula, numero;
+        do {
+            System.out.print(
+                    "Escreva uma senha que contenha um número, uma letra minuscula, maiscula e um dos caracteres especiais('#','*','-','!','@'): ");
+            senha = in.next();
+            especial = false;
+            maiuscula = false;
+            minuscula = false;
+            numero = false;
 
-do{
-     System.out.print("Escreva uma senha que contenha um número, uma letra minuscula, maiscula e um dos caracteres especiais('#','*','-','!','@'): ");
-    senha = in.nextLine();
-    boolean especial = false;
-    boolean maiuscula = false;
-    boolean minuscula = false;
-    boolean numero = false;
-
-    if(senha.contains("#") || senha.contains("*") || senha.contains("-") || senha.contains("!") || senha.contains("@")){
-        especial = true;
-    }
-
-    for(int i = 0; i < senha.length(); i++){
-        char letra = senha.charAt(i);
-        int num = senha.charAt(i);
-
-        if(senha.toUpperCase(letra)){
-            maiuscula = true;
-                
-            } else {
-                maiuscula = false;
+            if (senha.contains("#") || senha.contains("*") || senha.contains("-") || senha.contains("!")
+                    || senha.contains("@")) {
+                especial = true;
             }
 
-            if(senha.toLowerCase(letra)){
-                minuscula = true;
-            } else {
-                minuscula = false;
-            }
+            for (int i = 0; i < senha.length(); i++) {
+                char letra = senha.charAt(i);
 
-            if(senha.)
-            
-        }
-    } while();
+                if (Character.isUpperCase(letra)) {
+                    maiuscula = true;
+                }
+                if (Character.isLowerCase(letra)) {
+                    minuscula = true;
+                }
+                if (Character.isDigit(letra)) {
+                    numero = true;
+                }
+            }
+        } while (!(especial && maiuscula && minuscula && numero));
+
+        System.out.println("\nCadastro realizado com sucesso!");
+        System.out.println("---------------------------------");
+        System.out.println("Nome  |" + nome);
+        System.out.println("Idade |" + idade);
+        System.out.println("Email |" + email);
+        System.out.println("Senha |" + senha);
+
+        in.close();
 
     }
 
