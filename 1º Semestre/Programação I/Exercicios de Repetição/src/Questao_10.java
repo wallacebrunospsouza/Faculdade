47847a8199a7e87a035596bd13726df37ef4dfe6
Wrube <wallacebrunospsouza82@gmail.com>
2025-07-03 07:23:47 -0300

    corrige a lógica para determinar o maior e o menor número na classe Questao_10 (tenho que corrigir)

--- 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_10.java
+++ 1º Semestre/Programação I/Exercicios de Repetição/src/Questao_10.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 
 public class Questao_10 {
-    public static void main(String []args){
-        double Num, maior, menor;
+    public static void main(String[] args) {
+        double Num, maior = 0, menor = 0;
         int i;
         Scanner in = new Scanner(System.in);
 
-        for(i = 1; i <= 4; i++){
+        for (i = 1; i <= 4; i++) {
             System.out.print("Escreva um número:");
             Num = in.nextDouble();
 
-            if(i==1){
+            if (i == 1) {
                 maior = Num;
                 menor = Num;
-            } else {
-                if (menor > maior){
-                menor = maior;
-                maior = menor;
+            } else if(i > 1) {
+                if (menor > maior) {
+                    menor = maior;
+                    maior = menor;
                 }
+                if (maior > menor){
+                    maior = maior;
+                    menor = menor;
+                }
+
+            }
         }
+        System.out.printf("Seu maior é: %.2f\nSeu menor é: %.2f", maior, menor);
+
     }
 
-}
 }
