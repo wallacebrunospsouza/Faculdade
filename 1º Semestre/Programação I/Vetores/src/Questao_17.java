4de3904c7840faf9a0bdf34d8fb5bddc580331d1
Wrube <wallacebrunospsouza82@gmail.com>
2025-08-11 22:31:09 -0300

    TERMINAR!!

---
+++ 1º Semestre/Programação I/Vetores/src/Questao_17.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Questao_17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int flecha[] = new int[3];
+
+        for(int i = 0; i < flecha.length; i++){
+            System.out.println("Escreva");
+        }
+
+    }
+
+}
