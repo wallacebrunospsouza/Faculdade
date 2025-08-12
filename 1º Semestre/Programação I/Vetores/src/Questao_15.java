fe256e7dea03a375d17de79b00b20133808a3048
Wrube <wallacebrunospsouza82@gmail.com>
2025-08-11 21:55:03 -0300

    Refatora a classe Questao_15 para incluir método de pesquisa em vetor e melhorar a lógica de cadastro de contas

--- 1º Semestre/Programação I/Vetores/src/Questao_15.java
+++ 1º Semestre/Programação I/Vetores/src/Questao_15.java
@@ -1,31 +1,71 @@
 import java.util.Scanner;
 
-public class Questao_15{
-    public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        String nome[] = new String[3];
-        int CONTAS[] = new int[3];
+public class Questao_15 {
+    static Scanner in = new Scanner(System.in);
 
-        for (int i = 0; i < CONTAS.length; i++) {
-            do{
-                System.out.print("\nCadastre seu nome: ");
-                nome[i] = in.next();
-            System.out.print("\nCadastre o código da conta: ");
-            CONTAS[i] = in.nextInt();
-            if(CONTAS[i] < 0){
-                System.out.println("Código inválido!!");
+    public static int pesquisaEmVetor(int[] v, int num) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == num) {
+                return i;
             }
-            }while(CONTAS[i] < 0);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int contas[] = new int[3];
+        double saldos[] = new double[3];
+        int v[] = new int[3];
+        double v1[] = new double[3];
+        for (int i = 0; i < v.length; i++) {
+            int pos, codConta;
+            do {
+                System.out.println("Digite o valor do código da conta: ");
+                codConta = in.nextInt();
+                pos = pesquisaEmVetor(v, codConta);
+                if (pos != -1) {
+                    System.out.println("Conta já existente!");
+                }
+            } while (pos != -1);
+            v[i] = codConta;
+
+            System.out.println("Digite o valor do saldo da conta: ");
+            v1[i] = in.nextDouble();
         }
 
-        System.out.println("=============");
-        System.out.println("\tMenu");
-        System.out.println("=============");
-        System.out.println("1 - Depositar");
-        System.out.println("2 - Sacar");
-        System.out.println("3 - Consultar Saldo");
-        System.out.println("4 - Tchau brigado!");
-        System.out.println("=============");
-        System.out.println("Digite a opção desejada: ");
+        int op, codConta, pos;
+        do {
+            System.out.println("=============");
+            System.out.println("\tMenu");
+            System.out.println("=============");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar Saldo");
+            System.out.println("4 - Tchau brigado!");
+            System.out.println("=============");
+            System.out.println("Digite a opção desejada: ");
+            op = in.nextInt();
+            if (op == 1) {
+                System.out.println("Digite a conta a ser depositada: ");
+                codConta = in.nextInt();
+                pos = pesquisaEmVetor(contas, codConta);
+                if (pos == -1) {
+                    System.out.println("Conta inexistente!");
+                } else {
+                    double deposito;
+                    System.out.println("Digite o valor a ser depositado: ");
+                    deposito = in.nextDouble();
+                    saldos[pos] += deposito;
+                    System.out.println("Saldo atual: " + saldos[pos]);
+                }
+            } else if (op == 2) {
+                System.out.println("Sacar");
+            } else if (op == 3) {
+                System.out.println("Consultar Saldo");
+            } else if (op != 4) {
+                System.out.println("Opção inválida! ");
+            }
+        } while (op != 4);
+
     }
 }
