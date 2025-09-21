a1a6c60d4de60fd17d143602c599ecae20f86c82
Wrube <wallacebrunospsouza82@gmail.com>
2025-09-21 09:27:24 -0300

    autalizcionss

---
+++ 2° Semestre/SUBROTINAS/src/Questao_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Questao_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Escreva um número: ");
+        numero = in.nextInt();
+        isZero(numero);
+    }
+
+    static float isZero(float num){
+        if(num != 0){
+            System.out.println("Relaxa pae, seu número não é nulo");
+        } else{
+            System.out.println("Vishe fio, é nulo");
+        }
+   return num;
+}
+}
---
+++ 2° Semestre/SUBROTINAS/src/Questao_4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Questao_4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Escreva um número: ");
+        numero = in.nextInt();
+        isEven(numero);
+    }
+
+   public static void isEven(int num){
+    if(num % 2 == 0){
+        System.out.println("Seu número é par");
+    }else{
+        System.out.println("É ímpar");
+    }
+    }
+
+}
